d1f5937d399f9ef151c65cdd2c23d5d527853dff
Beni Fissha <benifissha@gmail.com>
2023-11-29 21:15:47 +0300

    initial commit

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -10,9 +10,9 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
     binary_tree_t *newnode;
-    newnode = binary_tree_node(parent, value);
     if (parent == NULL)
         return NULL;
+    newnode = binary_tree_node(parent, value);
     if (!newnode)
         return NULL;
     if (parent->left)
