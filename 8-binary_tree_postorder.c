959e3531f6542fe5ebededa01bf25fb90829b392
Beni Fissha <benifissha@gmail.com>
2023-11-30 11:03:02 +0300

    updated

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -9,9 +9,9 @@
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
     if (!tree)
-        return NULL;
+        return;
     if (!func)
-        return NULL;
+        return;
     binary_tree_postorder(tree->left, func);
     binary_tree_postorder(tree->right, func);
     func(tree->n);
