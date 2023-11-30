6ec42c86480c02da47a63e282052286ce07641a5
Beni Fissha <benifissha@gmail.com>
2023-11-30 13:05:34 +0300

    initial commit

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree
+ * 
+ * @tree: is a pointer to the root node of the tree
+ * 
+ * Return: returns the number of nodes
+*/
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    size_t nodes = 0;
+    if(!tree)
+        return (0);
+    if (!tree->left && !tree->right)
+        return (0);
+    if (tree->left || tree->right)
+        nodes = 1;
+    nodes = nodes + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
+    return(nodes);
+}
