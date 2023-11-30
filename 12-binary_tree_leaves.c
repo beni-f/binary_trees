3102b60edd7d1bfadc6ef1ebb005f348ceb9f82d
Beni Fissha <benifissha@gmail.com>
2023-11-30 12:17:31 +0300

    updated

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * 
+ * @tree: a pointer to the root node of the tree
+ * 
+ * Return: returns the number of leaves
+*/
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
     if (!tree)
